0996ecd807ea4012961950bad76644706291f439
31431 <kitsada280@gmail.com>
2016-12-24 14:03:31 +0800

    Add patch to update data

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 const {mongoose} = require('./db/mongoose.js');
 const {Todo} = require('./models/todo.js');
@@ -63,6 +64,32 @@ app.delete('/todos/:id',(req,res)=>{
 		res.status(200).send({todo});
 	}).catch((e)=>{
 		res.status(404).send();
+	})
+});
+
+app.patch('/todos/:id', (req,res)=>{
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text','completed']);
+
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	if(_.isBoolean(body.completed) && body.completed){
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id,{$set:body},{new: true}).then((todo)=>{
+		if(!todo){
+			return res.status(404).send();
+		}
+		res.send({todo});
+
+	}).catch((e)=>{
+		res.status(400).send();
 	})
 })
 
